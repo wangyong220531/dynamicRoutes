afa73c169bbb49af1e8b814e746e64bdac426479
王勇 <wy19945372694@163.com>
2023-02-15 16:32:54 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { BrowserRouter, Routes, Route, useNavigate, useParams } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useNavigate, useParams, Navigate } from "react-router-dom"
 import "./App.css"
 import Profile from "./pages/profile"
 import DetailA from "./pages/detailA"
 import DetailB from "./pages/detailB"
+import NotFound from "./pages/notFound"
+import Home from "./pages/home"
 
 function Detail() {
     let { path } = useParams()
@@ -18,9 +20,12 @@ export default function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Navigate to="/home" />} />
+                <Route path="/home" element={<Home />} />
                 <Route path="/profile" element={<Profile />}>
                     <Route path=":path" element={<Detail />}></Route>
                 </Route>
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     )
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom"
 export default function Profile() {
     return (
         <>
+            <div>我是主页</div>
             <Outlet />
         </>
     )
